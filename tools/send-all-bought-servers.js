e0cedd789d640b1bfda05a97e3cb8b5dd4539a1b
ywn <98105137+ywn197@users.noreply.github.com>
2023-02-20 22:50:22 +0900

    Create  send-all-bought-servers.js

---
+++ tools/send-all-bought-servers.js
@@ -0,0 +1,12 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    if(ns.args.length === 0)
+        ns.tprint("please input file(s) name into argument");
+    let fileNames = ns.args;
+    let serverList =  ns.getPurchasedServers()
+
+    for(const i in serverList){
+        ns.scp(fileNames,serverList[i]);
+    }
+    ns.tprint("sended");
+}
