c8a47381a36a1bb28e90c29f9c4038ddfea9be76
ywn <98105137+ywn197@users.noreply.github.com>
2023-02-20 22:49:10 +0900

    Create run-all-bought-servers.js

---
+++ tools/run-all-bought-servers.js
@@ -0,0 +1,13 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    let sctipt = ns.args[0];
+    let serverList = ns.getPurchasedServers();
+    for(let i in serverList){
+        let th = ns.getServerMaxRam(serverList[i]) / ns.getScriptRam(sctipt);
+        if(th <= 0){
+            ns.tprint("server " + serverList[i] + " can't run this script because its ram is too small");           
+            continue;
+       }
+        ns.exec(sctipt,serverList[i],385);
+    }
+}
