b2c3a4dabaa0b85db60cd874127ca9bbe7cd3e79
ywn <98105137+ywn197@users.noreply.github.com>
2023-03-09 22:11:20 +0900

    Update send-all-controled-servers.js
    Fixed a bug where servers behind servers with no money were not found.

--- tools/send-all-controled-servers.js
+++ tools/send-all-controled-servers.js
@@ -1,18 +1,27 @@
-//This sctipt sends a file to the server you have root access.(Excluding purchased servers)
+//This script runs a sctipt on the server you have root acccess(Excluding purchased servers.).
 /** @param {NS} ns */
 let ns;
 export async function main(_ns) {
+    CONST BOUGHT_SERVER_DOMEIN = "ywn";
     ns = _ns;
     if(ns.args <= 0){
-        ns.tprint("please tyep a file name to send in argument");
+        ns.tprint("please tyep a script name to run in argument");
         return;
     }
 
-    let file = ns.args[0];
+    let script = ns.args[0];
     let serverList = await createHostList("home","false-image-server");
+    
+    ns.tprint(serverList);
 
     for(let i in serverList){
-        ns.scp(file,serverList[i]);
+        let th = Math.floor(ns.getServerMaxRam(serverList[i]) / ns.getScriptRam(script));
+        if(th <= 0){
+            ns.tprint("Server " + serverList[i] + " can't run this script becouse its ram is too small");
+            continue;
+        }
+        ns.exec(script,serverList[i],th);
+
     }
     
 
@@ -26,21 +35,30 @@ async function createHostList(host,connectFrom) {
     
     //Delete source server
     for(let i = 0;i < gotList.length;i ++){
+        let serv = gotList[i];
         if (gotList[i] == connectFrom){ 
             gotList.splice(i,1);
             i --;
-        } else if (ns.getServerMaxMoney(gotList[i]) === 0){ 
-            gotList.splice(i,1);
-            i --;
-        } else if (!ns.hasRootAccess(gotList[i])){ 
+        }  else if (!ns.hasRootAccess(gotList[i])){ 
+             gotList.splice(i,1);
+             i --;
+        } else if(gotList[i].substring(gotList[i].length - 3) === BOUGHT_SERVER_DOMEIN){
              gotList.splice(i,1);
              i --;
         }
-
     }
+    let searchingLIst = gotList.slice();
+
+    for(let i in gotList){
+        if (ns.getServerMaxMoney(gotList[i]) === 0){ 
+            gotList.splice(i,1);
+            i --;
+        }
+    }
+
     list = list.concat(gotList);
-    for(const i in gotList){
-       list = list.concat(await createHostList(gotList[i],host)); 
+    for(const i in searchingLIst){
+       list = list.concat(await createHostList(searchingLIst[i],host)); 
     }
     
     return(list);
